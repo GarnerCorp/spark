91738ef37e7f84deddc2dbed44bc7ca812389060
A-Ploeg <attila.vanderploeg@garnercorp.com>
2021-03-05 10:58:19 -0500

    Add postgres hack

--- garner/scripts/copy_files_in.sh
+++ garner/scripts/copy_files_in.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
+
+source ${0:a:h}/garner.env
 echo "Copying new distribution"
 cp -r  ${SPARK_PARENT_LOCATION}/dist ${SPARK_PARENT_LOCATION}/garner
+wait
+echo "Copying in postgresql"
+cp -r  ${SPARK_PARENT_LOCATION}/garner/postgres-jar-hack/* ${SPARK_PARENT_LOCATION}/garner/dist/jars
 wait
 echo "Copying in conf"
 cp -r  ${SPARK_PARENT_LOCATION}/conf ${SPARK_PARENT_LOCATION}/garner/dist
