ee38d8d546f15f3f988e96664e91af835f8c6d6d
A-Ploeg <attila.vanderploeg@garnercorp.com>
2021-03-11 17:46:14 -0500

    Minor modification to build scripts

--- garner/scripts/copy_files_in.sh
+++ garner/scripts/copy_files_in.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 source ${0:a:h}/garner.env
+echo "Removing old distrubution"
+rm -r ${0:a:h}/../../garner/dist
 echo "Copying new distribution"
 cp -r  ${SPARK_PARENT_LOCATION}/dist ${SPARK_PARENT_LOCATION}/garner
 wait
--- garner/scripts/make_distribution.sh
+++ garner/scripts/make_distribution.sh
@@ -3,7 +3,4 @@ source ${0:a:h}/garner.env
 
 /bin/bash ${SPARK_PARENT_LOCATION}/dev/make-distribution.sh  --name garner-spark --tgz -Phive -Phive-thriftserver -Pmesos -Pyarn -Pkubernetes -Pscala-2.13;
 wait
-echo "Removing old distrubution"
-rm -r ${0:a:h}/../../garner/dist
-wait
 /bin/bash ${0:a:h}/copy_files_in.sh
