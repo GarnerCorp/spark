70ee57135aa87bc57673d8d99d791d8618251d49
A-Ploeg <attila.vanderploeg@garnercorp.com>
2021-04-23 16:17:12 -0400

    Add extra core to executor

--- garner/docker/docker-entrypoint-spark-hiveThriftServer2.sh
+++ garner/docker/docker-entrypoint-spark-hiveThriftServer2.sh
@@ -7,7 +7,7 @@ if [[ -z ${LIGHTHOUSE_URL} ]]; then
  export LIGHTHOUSE_URL=TEST_LIGHTHOUSE_INTERNAL_BACKEND_PORT
 fi
 
-/bin/bash $SPARK_HOME/sbin/start-thriftserver.sh --total-executor-cores 1 --master $SPARK_MASTER_URL \
+/bin/bash $SPARK_HOME/sbin/start-thriftserver.sh --total-executor-cores 2 --master $SPARK_MASTER_URL \
   --hiveconf javax.jdo.option.ConnectionURL=$SPARK_METASTORE_URL \
   --hiveconf javax.jdo.option.ConnectionUserName=$METASTORE_USER \
   --hiveconf javax.jdo.option.ConnectionPassword=$METASTORE_PASSWORD \
